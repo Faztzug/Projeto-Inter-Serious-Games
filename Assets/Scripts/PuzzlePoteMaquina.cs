0855d3cd7e80c5c0d3235e6d177583b9682dc6cf
guilhermeburgheri <guilherme.burgheri@hotmail.com>
2021-11-17 09:51:45 -0300

    Implementando o puzzle de coleta de terra.

--- Assets/Scripts/PuzzlePoteMaquina.cs
+++ Assets/Scripts/PuzzlePoteMaquina.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 
 public class PuzzlePoteMaquina : MonoBehaviour
 {
+    public GameObject itemObject;
+    private Item itemClass;
+    private string prefabName;
+
+    private void Start()
+    {
+        itemClass = itemObject.GetComponent<Item>();
+        prefabName = itemClass.itemName;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            gameObject.SetActive(false);    
+            Debug.Log("Player entrou no trigger");
+            Inventory inventario = collision.GetComponent<Inventory>();
+
+            if (inventario.selectedItem != null && inventario.selectedItem.itemName == prefabName)
+            {
+                Debug.Log("itens Iguais");
+                gameObject.SetActive(false);
+                inventario.EliminarItemSelecionado();
+            }
         }
     }
 }
